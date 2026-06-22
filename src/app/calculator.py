3bbc63ff2a324a75c54c504b261e3baf0971807d
Alyaev Dolir <worlddolir@gmail.com>
2026-06-22 09:54:39 +0300

    fixes

--- src/app/calculator.py
+++ src/app/calculator.py
@@ -1,13 +1,23 @@
-import math, sys
+import math
+import sys
 from datetime import datetime
-def Calc_Func( x,y, op ):
-    if op=='add':return x+y
-    elif op=='sub':
-        return x-y
-    elif op=='mul':
-        return x*y
-    elif op=='div':
-        if y==0: return None
-        return x/y
-    else: return 0
-print( Calc_Func(10,5, 'add'))
+
+
+def calculate_value(x: float, y: float, op: str) -> float | None:
+    """Example of a docstring."""
+    if op == "add":
+        return x + y
+    elif op == "sub":
+        return x - y
+    elif op == "mul":
+        return x * y
+    elif op == "div":
+        if y == 0:
+            raise ValueError("Division by zero")
+            # return None
+        return x / y
+    else:
+        return 0
+
+
+print(calculate_value(10, 5, "add"))
--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,11 @@
+from app.logger import log
+
+
 def main() -> None:
     print("App running")
+    log.warning('warning message')
+    log.info('info message')
+    log.debug('debug message')
 
 
 if __name__ == "__main__":
